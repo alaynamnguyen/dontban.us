86a9b4d428d3a39a45289f1f70f85f7dbd378e2e
elowop <42976129+elowop@users.noreply.github.com>
2023-09-09 13:42:58 -0400

    changed sentiment logic to accept string

--- sentiment.py
+++ sentiment.py
@@ -2,16 +2,16 @@ import pickle
 from nltk.tokenize import word_tokenize
 from trainModel import remove_noise
 
-def main():
+def get_sentiment(custom_sentence):
     # Load the saved classifier from the file
     with open('sentimentClassifier.pickle', 'rb') as f:
         loaded_classifier = pickle.load(f)
 
-    custom_tweet = "I ordered just once from TerribleCo, they screwed up, never used the app again."
-
-    custom_tokens = remove_noise(word_tokenize(custom_tweet))
-
-    print(custom_tweet, loaded_classifier.classify(dict([token, True] for token in custom_tokens)))
+    custom_tokens = remove_noise(word_tokenize(custom_sentence))
+    
+    return loaded_classifier.classify(dict([token, True] for token in custom_tokens))
 
 if __name__ == "__main__":
-    main()
+    test_sentence = "this is a happy sentence with a bad word like shit"
+
+    print(get_sentiment(test_sentence))
