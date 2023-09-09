5367806aa98b9505445e4e40d74054d087b188d0
Alayna Nguyen <32422836+alaynamnguyen@users.noreply.github.com>
2023-09-09 13:56:51 -0400

    Added main file

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import pickle
-from nltk.tokenize import word_tokenize
-from trainModel import remove_noise
-
-def get_sentiment(loaded_classifier, message):
-    # Returns sentiment (Positive, Neutral, Negative) for a message
-    custom_tokens = remove_noise(word_tokenize(message))
-    return loaded_classifier.classify(dict([token, True] for token in custom_tokens))
+from sentiment import get_sentiment
+from niceify import reword_phrase
 
 def main():
     # Load the saved classifier from the file
     with open('sentimentClassifier.pickle', 'rb') as f:
         loaded_classifier = pickle.load(f)
+
 
     messages = ["I ordered just once from TerribleCo, they screwed up, never used the app again.",
                      "You're such a loser why do you even play this game.",
@@ -18,9 +14,14 @@ def main():
                      "My grandmother plays better than you and she can't even see without glasses"]
 
     for message in messages:
-        print(message, get_sentiment(loaded_classifier, message))
+        sentiment = get_sentiment(loaded_classifier, message)
+        if sentiment == "Negative":
+            print("Negative message!! Rewording with GPT:")
+            reworded_message = reword_phrase(message)
+            print(reworded_message)
+        else:
+            print("Positive message!! Keeping as is:")
+            print(message)
 
 if __name__ == "__main__":
-    test_sentence = "this is a happy sentence with a bad word like shit"
-
-    print(get_sentiment(test_sentence))
+    main()
